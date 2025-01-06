156a5e61c84f526cef9de6375ba4eee56e4e109c
MLYTC1 <kechexmadze51@gmail.com>
2025-01-06 22:32:18 +0400

    Profile Form, Controller & Notifications Are Ready

--- users/forms.py
+++ users/forms.py
@@ -1,5 +1,5 @@
 from django import forms 
-from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
+from django.contrib.auth.forms import AuthenticationForm, UserCreationForm, UserChangeForm
 
 from users.models import User
 
@@ -12,9 +12,8 @@ class UserLoginForm(AuthenticationForm):
         
     username = forms.CharField()
     password = forms.CharField()
-    
-    
-    
+
+
 class UserRegistrationForm(UserCreationForm):
 
     class Meta:
@@ -35,14 +34,55 @@ class UserRegistrationForm(UserCreationForm):
     password1 = forms.CharField()
     password2 = forms.CharField()
 
-    
-    
-    
-    
-    
-    
-    
-    
+
+class ProfileForm(UserChangeForm):
+    class Meta:
+        model = User 
+        fields = (
+            "image",
+            "first_name",
+            "last_name",
+            "username",
+            "email",
+            )
+
+    image = forms.ImageField(required=False)
+    first_name = forms.CharField()
+    last_name = forms.CharField()
+    username = forms.CharField()
+    email = forms.CharField()
+
+    # class UserProfileView(LoginRequiredMixin, CacheMixin , UpdateView):
+    # template_name = 'users/profile.html'
+    # form_class = ProfileForm
+    # success_url = reverse_lazy('users:profile')
+
+    # def get_object(self, queryset=None):
+    #     return self.request.user
+
+    # def form_valid(self, form):
+    #     messages.success(self.request, "Профайл успешно обновлен")
+    #     return super().form_valid(form)
+
+    # def form_invalid(self, form):
+    #     messages.error(self.request, "Произошла ошибка")
+    #     return super().form_invalid(form)
+
+    # def get_context_data(self, **kwargs):
+    #     context = super().get_context_data(**kwargs)
+    #     context['title'] = 'Home - Кабинет'
+
+    #     # Можно вынести сам запрос в отдельный метод этого класса контроллера
+    #     orders = Order.objects.filter(user=self.request.user).prefetch_related(
+    #             Prefetch(
+    #                 "orderitem_set",
+    #                 queryset=OrderItem.objects.select_related("product"),
+    #             )
+    #         ).order_by("-id")
+
+    #     context['orders'] = self.set_get_cache(orders, f"user_{self.request.user.id}_orders", 60)
+    #     return context
+
     # username = forms.CharField(
     #     label='Name',
     #     widget=forms.TextInput(attrs={  "autofocus": True,
@@ -53,4 +93,3 @@ class UserRegistrationForm(UserCreationForm):
     #     widget=forms.PasswordInput(attrs={  "autocomplete": "current-password",
     #                                         'class': 'form-control',
     #                                         'placeholder': "Input Password"}))
-
--- users/views.py
+++ users/views.py
@@ -1,8 +1,9 @@
+from django.contrib.auth.decorators import login_required
 from django.http import HttpResponseRedirect
 from django.shortcuts import redirect, render
-from django.contrib import auth
+from django.contrib import auth, messages
 from django.urls import reverse
-from users.forms import UserLoginForm, UserRegistrationForm
+from users.forms import ProfileForm, UserLoginForm, UserRegistrationForm
 
 from users.forms import UserCreationForm
 
@@ -15,6 +16,7 @@ def login(request):
             user = auth.authenticate(username=username, password=password)
             if user:
                 auth.login(request, user)
+                messages.success(request, f"{username}, You are Logged In")
                 return HttpResponseRedirect(reverse('main:index'))
     else:
         form = UserLoginForm()
@@ -31,6 +33,7 @@ def registration(request):
         if form.is_valid():
             form.save()
             user = form.instance
+            messages.success(request, f"{user.username}, You are Succesfully Registered")
             auth.login(request, user)
             return HttpResponseRedirect(reverse("main:index"))
     else:
@@ -42,15 +45,27 @@ def registration(request):
     }
     return render(request, "users/registration.html", context)
 
-
+@login_required
 def profile(request):
+    if request.method == "POST":
+        form = ProfileForm(data=request.POST, instance=request.user, files=request.FILES)
+        if form.is_valid():
+            form.save()
+            messages.success(request, "Profile Succesfully Updated")
+            return HttpResponseRedirect(reverse('user:profile'))
+    else:
+        form = ProfileForm(instance=request.user)
+
     context = {
         "title": "Home - Profile",
+        "form":form
         
     }
     return render(request, "users/profile.html", context)
 
 
+@login_required
 def logout(request):
+    messages.success(request, f"{request.user.username}, You are Logged Out")
     auth.logout(request)
     return redirect(reverse('main:index'))
