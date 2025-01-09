85cdd2919a8425c6a28aa267478de96e249dd72a
MLYTC1 <kechexmadze51@gmail.com>
2025-01-10 00:10:10 +0400

    sqlite

--- app/settings.py
+++ app/settings.py
@@ -83,14 +83,21 @@ WSGI_APPLICATION = 'app.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'home',
+#         'USER': 'home',
+#         'PASSWORD':'home',
+#         'HOST': 'localhost',
+#         'PORT': '5432',
+#     }
+# }
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'home',
-        'USER': 'home',
-        'PASSWORD':'home',
-        'HOST': 'localhost',
-        'PORT': '5432',
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR
+        / "db.sqlite3",  # This will create a SQLite database file in your project's base directory
     }
 }
 
