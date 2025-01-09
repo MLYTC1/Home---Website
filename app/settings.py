dbf85d37cede3474aedb941226fb48bd7238b2d6
MLYTC1 <kechexmadze51@gmail.com>
2025-01-10 00:25:00 +0400

    herocu hosting needs

--- app/settings.py
+++ app/settings.py
@@ -83,23 +83,17 @@ WSGI_APPLICATION = 'app.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': 'home',
-#         'USER': 'home',
-#         'PASSWORD':'home',
-#         'HOST': 'localhost',
-#         'PORT': '5432',
-#     }
-# }
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR
-        / "db.sqlite3",  # This will create a SQLite database file in your project's base directory
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'home',
+        'USER': 'home',
+        'PASSWORD':'home',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
+
 
 
 # Password validation
